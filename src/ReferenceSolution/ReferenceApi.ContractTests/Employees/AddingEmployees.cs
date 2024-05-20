36f55ed9a73e32a374760d0f58ef2f05b0ef0033
Jeff Gonzalez <jeff@hypertheory.com>
2024-05-20 12:33:59 -0400

    before lunch Monday

---
+++ src/ReferenceSolution/ReferenceApi.ContractTests/Employees/AddingEmployees.cs
@@ -0,0 +1,41 @@
+﻿
+using Alba;
+using ReferenceApi.Employees;
+
+namespace ReferenceApi.ContractTests.Employees;
+public class AddingEmployees
+{
+
+    [Fact]
+    public async Task Bannana()
+    {
+        // Given
+        // A Host Per Test (Host Per Class, Collections)
+        var request = new EmployeeCreateRequest
+        {
+            FirstName = "Boba",
+            LastName = "Fett"
+        };
+
+        var expected = new EmployeeResponseItem
+        {
+
+            Id = "fett-boba",
+            FirstName = "Boba",
+            LastName = "Fett"
+        };
+        var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api =>
+        {
+            api.Post.Json(request).ToUrl("/employees");
+            api.StatusCodeShouldBe(201);
+        });
+
+        var responseMessage = await response.ReadAsJsonAsync<EmployeeResponseItem>();
+        Assert.NotNull(responseMessage);
+
+        Assert.Equal(expected, responseMessage);
+
+    }
+}
---
+++ src/ReferenceSolution/ReferenceApi/Employees/Api.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReferenceApi.Employees;
+
+public class Api : ControllerBase
+{
+    [HttpPost("employees")]
+
+    public async Task<ActionResult> AddEmployeeAsync(
+        [FromBody] EmployeeCreateRequest request)
+    {
+        return StatusCode(201);
+    }
+}
+
+public record EmployeeCreateRequest
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+}
+
+public record EmployeeResponseItem
+{
+    public required string Id { get; set; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+}
