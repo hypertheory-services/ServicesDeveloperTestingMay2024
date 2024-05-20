eb1af7686f9e9b7011e43cfe1d83199ae69bd087
Jeff Gonzalez <jeff@hypertheory.com>
2024-05-20 15:12:10 -0400

    before break

---
+++ src/ReferenceSolution/ReferenceApi.UnitTests/SlugGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿
+using ReferenceApi.Employees;
+
+namespace ReferenceApi.UnitTests;
+public class SlugGeneratorTests
+{
+
+    [Theory]
+    [InlineData("Boba", "Fett", "fett-boba")]
+    [InlineData("Luke", "Skywalker", "skywalker-luke")]
+    [InlineData("Joe", "", "joe")]
+    [InlineData("Cher", "", "cher")]
+
+    public void Avacado(string firstName, string lastName, string expected)
+    {
+        // Given
+        var slugGenerator = new EmployeeSlugGenerator();
+
+
+        // When
+        string slug = slugGenerator.Generate(firstName, lastName);
+
+
+        // Then
+        Assert.Equal(expected, slug);
+    }
+}
---
+++ src/ReferenceSolution/ReferenceApi/Employees/EmployeeSlugGenerator.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ReferenceApi.Employees;
+
+public class EmployeeSlugGenerator
+{
+    public string Generate(string firstName, string lastName)
+    {
+        if (string.IsNullOrEmpty(lastName))
+        {
+            return firstName.ToLowerInvariant();
+        }
+        return $"{lastName.ToLowerInvariant()}-{firstName.ToLowerInvariant()}";
+    }
+}
