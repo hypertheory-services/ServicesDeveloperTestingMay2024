8546a94543116314a8fcbd2273310abd9bb94558
Jeff Gonzalez <jeff@hypertheory.com>
2024-05-21 15:37:16 -0400

    test containers

--- src/ReferenceSolution/ReferenceApi.ContractTests/HostFixture.cs
+++ src/ReferenceSolution/ReferenceApi.ContractTests/HostFixture.cs
@@ -1,5 +1,6 @@
 ﻿using Alba;
 using Microsoft.AspNetCore.TestHost;
+using Testcontainers.PostgreSql;
 
 
 namespace ReferenceApi.ContractTests;
@@ -7,20 +8,29 @@ public class HostFixture : IAsyncLifetime
 {
     public IAlbaHost Host = null!;
 
+    private PostgreSqlContainer _container = new PostgreSqlBuilder()
+        .WithImage("postgres:16.2-bullseye")
+        .WithUsername("testuser")
+        .WithPassword("tacosalad")
+        .WithDatabase("reference")
+        .Build();
+
     public async Task InitializeAsync()
     {
+
+        await _container.StartAsync();
         Host = await AlbaHost.For<Program>(config =>
         {
+            config.UseSetting("ConnectionStrings:data", _container.GetConnectionString());
             config.ConfigureTestServices(services =>
             {
-                //var testFakeButtress = Substitute.For<ICheckForUniqueEmployeeStubs>();
-                //testFakeButtress.CheckUniqueAsync(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(true);
-                //services.AddScoped<ICheckForUniqueEmployeeStubs>(sp => testFakeButtress);
+                // fake database calls.
             });
         });
     }
     public async Task DisposeAsync()
     {
+        await _container.StopAsync();
         await Host.DisposeAsync();
     }
 
